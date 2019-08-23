4829fd384153130d5849169460ed696dbf519d40
GD7 <GD7@DESKTOP-58VHOFH>
2019-08-23 17:54:05 +0900

    수정본

---
+++ src/main/java/com/java/hdfs/Map.java
@@ -0,0 +1,35 @@
+package com.java.hdfs;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+//Mapper<(입력키<행번호> : 입력값<행의글자>) , (출력키<글자> : 출력값<1>)>
+public class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+	// 출력 키 변수
+	protected Text textKey = new Text();
+	// 출력 값 변수
+	@Override
+	protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, IntWritable>.Context context) throws IOException, InterruptedException {
+		
+		String[] values = value.toString().split(",");
+		
+		String Month =values[1];
+		int DepDelay=0;
+		// 출력 키에 넣을 문자열 변수
+		String strKey = values[0];
+		if(!("NA").equals(values[15])) { 
+			DepDelay = Integer.parseInt(values[15]); }
+			// 출력 키에 문자열 변수 적용
+	
+			textKey.set(strKey + Month);
+			// 전체 결과 출력하기
+			IntWritable intValue = new IntWritable(DepDelay);
+			context.write(textKey, intValue);
+		
+		}
+	}
+	
